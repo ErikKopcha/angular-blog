3f4c9fafa94bcbe71c2db3df547399c4335e12ba
Erik Kopcha <erickopcha@gmail.com>
2021-10-25 23:09:15 +0300

    feat: edit page, update post service

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostService} from "../../shared/components/post.service";
+import {switchMap} from "rxjs/operators";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private uSub!: Subscription;
+  public form!: FormGroup;
+  private post!: Post;
+  public submited: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private postServise: PostService
+  ) { }
+
+  ngOnDestroy(): void {
+    this.uSub.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postServise.getById(params['id']);
+      }))
+      .subscribe((post: Post) => {
+        this.post = post;
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required),
+        })
+      });
+  }
+
+  public submit() {
+    if (this.form.invalid) return;
+
+    this.submited = true;
+
+    this.postServise.update({
+      id: this.post.id,
+      text: this.form.value.text,
+      title: this.form.value.title,
+      author: this.post.author,
+      date: new Date()
+    }).subscribe(() => {
+      this.submited = false;
+    });
+  }
 }
